b761d6c476a21be3cdaeeb340dd011daaf772c9d
SyrusVir <jude.alnas@gmail.com>
2020-11-27 02:16:29 -0600

    Initial Commit of Qt Code
    Initial upload of Qt Designer practice code

---
+++ mainwindow.py
@@ -0,0 +1,56 @@
+from PyQt5 import QtWidgets, QtCore, uic
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
+import sys
+from random import randint
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        #Load the UI Page
+        uic.loadUi('mainwindow.ui', self)
+        
+        self.x = list(range(100))
+        self.y = [randint(0,100) for _ in range(100)]
+       
+        self.data_line = self.graphWidget.plot(self.x, self.y)
+       
+        self.pauseButton.toggled.connect(self.pausePlot)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def plot(self, x, y):
+        self.data_line = self.graphWidget.plot(x, y)
+    
+    def pausePlot(self):
+        if self.pauseButton.isChecked():
+            self.timer.stop()
+        else:
+            self.timer.start()
+
+    def update_plot_data(self):
+
+        self.x = self.x[1:]  # Remove the first x element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first y element
+        self.y.append(randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':         
+    main()
