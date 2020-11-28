8b548e03d2d6c7d1b2a82bc592bb909a1b7471fc
SyrusVir <jude.alnas@gmail.com>
2020-11-28 17:40:45 -0600

    Transmitting data from C to Py
    stamped_sine_server.c currently transmits the numbers of a sinusoid to mainwindow.py, which plots. No timestamp transmission yet. Currently transmits one number at a time rapidly, so there is a long buffer.

--- mainwindow.py
+++ mainwindow.py
@@ -3,7 +3,11 @@ from pyqtgraph import PlotWidget
 import pyqtgraph as pg
 import sys
 from random import randint
+import socket
+import struct
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = IPv4; SOCK_STREAM = TCP
+s.connect((socket.gethostname(), 49417))
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -40,7 +44,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
 
         self.y = self.y[1:]  # Remove the first y element
-        self.y.append(randint(0,100))  # Add a new random value.
+        rec_data = s.recv(10)
+        rec_data = float(rec_data.decode("utf-8"))
+        self.y.append(rec_data)  # Add a new random value.
 
         self.data_line.setData(self.x, self.y)  # Update the data.
 
