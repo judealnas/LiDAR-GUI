6781588dfb5160103d037529a715fd1b2ae62103
SyrusVir <jude.alnas@gmail.com>
2021-01-16 16:10:15 -0600

    Created signals and slots for MsgControl

--- MsgControl.py
+++ MsgControl.py
@@ -3,36 +3,42 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 
 class MsgControl(qtw.QWidget):
+
+    sig_send_msg = qtc.pyqtSignal(str)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.send_button = qtw.QPushButton('SEND', self)
+        self.send_button.setSizePolicy(qtw.QSizePolicy(qtw.QSizePolicy.Minimum,qtw.QSizePolicy.Minimum))
         self.send_lineedit = qtw.QLineEdit(self)
         self.rec_lineedit = qtw.QLineEdit(self)
         self.rec_lineedit.setEnabled(False)
 
-        # create form layout containing one lineedit for the send message
-        self.send_form = qtw.QFormLayout()
-        self.send_form.addRow('Msg to send', self.send_lineedit)
+        # palce lineedits together in form
+        self.form_layout = qtw.QFormLayout()
+        self.form_layout.addRow('Message',self.send_lineedit)
+        self.form_layout.addRow('Received', self.rec_lineedit)
 
-        #create another form layout containing the other lineedit for the received message
-        self.rec_form = qtw.QFormLayout()
-        self.rec_form.addRow('Received', self.rec_lineedit)
+        # place form above button; 
+        self.setLayout(qtw.QVBoxLayout()) #set main widget layout
+        self.layout().addLayout(self.form_layout) #add lineedits
+        self.layout().addWidget(self.send_button) #add button
 
-        #Place the two lineedits next to each other
-        self.lineedit_layout = qtw.QHBoxLayout()
-        self.lineedit_layout.addLayout(self.send_form)
-        self.lineedit_layout.addLayout(self.rec_form)
-
-        
-        self.setLayout(qtw.QVBoxLayout())
-        self.layout().addLayout(self.lineedit_layout)
-        self.layout().addWidget(self.send_button)
+        #connect signals
+        self.send_button.released.connect(self.sendMsg)
+        self.sig_send_msg.connect(self.setReceiveText) #DEBUG
 
         self.show()
 
-    def getSendTrigger(self):
+    def sendMsg(self):
+        self.sig_send_msg.emit(self.send_lineedit.text())
+
+    def getSendSignal(self):
         return self.send_button.released
+
+    def setReceiveText(self, text):
+        self.rec_lineedit.setText(text)
 
 if __name__ == "__main__":
     import sys
