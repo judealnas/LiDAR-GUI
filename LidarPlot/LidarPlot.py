87de4a1675130930bc3563b80e4f5c8cc8878dba
SyrusVir <jude.alnas@gmail.com>
2021-01-24 00:32:07 -0600

    Threaded logging done; must swith to QThreadPool

--- LidarPlot/LidarPlot.py
+++ LidarPlot/LidarPlot.py
@@ -7,13 +7,22 @@ from enum import Enum, auto
 import time
 import math
 import re
+from collections import deque
 
 #Ui_LidarPlot, baseClass = uic.loadUiType('Ui_LidarPlot.ui')
 #ui_builder = Ui_LidarPlot()
 
 class LidarPlot(qtw.QWidget):
     
+    #"External" signals meant to be connected to other modules
     sig_log_event = qtc.pyqtSignal(str)
+    ##########################################################
+
+    #"Internal" signals to invoke methods of LidarLogger
+    sig_read_last_n_lines = qtc.pyqtSignal(int)
+    sig_read_first_n_lines = qtc.pyqtSignal(int)
+    sig_write_line = qtc.pyqtSignal(str)
+    #####################################################
 
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -49,31 +58,26 @@ class LidarPlot(qtw.QWidget):
         self.plot_mode = LidarPlotMode.SCROLLING
         self.data_curve = self.plot_widget.plot()   # create data curve object
 
-        ## Configure Data Log Path #################
-        now = time.gmtime()
-        self.data_file = time.strftime('%Y_%m_%d',now) + '_data_file.csv'
-        self.data_folder = os.path.dirname(__file__)
-        self.data_path = os.path.join(self.data_folder,self.data_file)
-        
-        num = 1
-        while (os.path.exists(self.data_path)):
-            self.data_file = time.strftime('%Y_%m_%d',now) + '_data_file ({}).csv'.format(num)
-            self.data_folder = os.path.dirname(__file__)
-            self.data_path = os.path.join(self.data_folder,self.data_file)
-
-            num += 1
-        os.makedirs(self.data_path)
-        ##############################
-
         ## Instantiate logger object and threads
         self.data_logger = LidarLogger()
-
+        self.data_logger_thread = qtc.QThread(self)
         #####################################
+        
         ## Connecting signals ###############################
+        #GUI signals
         self.pause_button.released.connect(self.updateData)
         self.clear_button.released.connect(self.clearData)
         self.mode_button.released.connect(self.changeMode)
+
+        #Thread signals
+        self.sig_read_first_n_lines.connect(self.data_logger.readFirstNLines)
+        self.sig_read_last_n_lines.connect(self.data_logger.readLastNLines)
+        self.sig_write_line.connect(self.data_logger.writeLine)
         ###################################################
+
+        #Allocate objects to threads and start
+        self.data_logger.moveToThread(self.data_logger_thread)
+        self.data_logger_thread.start()
 
     def changeMode(self):
         if self.plot_mode == LidarPlotMode.SCROLLING:
@@ -86,8 +90,12 @@ class LidarPlot(qtw.QWidget):
     
     def updateData(self, data: qtc.QByteArray):
         y, epoch = self.parseData(data)
-        self.logData(y,epoch)
+        
+        #send data to LidarLogger
+        data_log_str = str(epoch) + ',' + str(y)
+        self.sig_write_line.emit(str(data_log_str))
 
+        #plot data
         self.buffer.append(y)
         self.x.append(epoch)
         if (len(self.buffer) >= self.buffer_size) and self.plot_mode == LidarPlotMode.SCROLLING:
@@ -123,31 +131,90 @@ class LidarPlot(qtw.QWidget):
         self.x = []
         self.plotData()
 
+    # Accessor methods for modularity
     def getLogSignal(self):
         return self.sig_log_event
+    ###############################
 
 class LidarLogger(qtc.QObject):
     '''
-    Class responsible for logging all plotted data to CSV
+    Class responsible for logging all plotted data to CSV. Meant to run in separate thread
     '''
-    sig_return = qtc.pyqtSignal()
+    sig_return = qtc.pyqtSignal(list)
 
-        
     default_file = 'data_file.csv'
     default_folder = os.path.dirname(__file__)
-    default_path = os.path.join(self.default_folder, self.default_file)
 
     def __init__(self,path=None,*args,**kwargs):
         super().__init__(*args,**kwargs)
+        
         if path == None:
-            self.data_path = default_path
-            pass
+            self.data_path = self.__defaultPath()
+        else:
+            self.data_path = path
 
+    def __defaultPath(self):
+        default_path = os.path.join(self.default_folder, time.strftime("%Y_%m_%d") + self.default_file)
+        default_path = self.findFileNum(default_path)
+        return default_path
+
+    def writeLine(self,msg: str):
+        with open(self.data_path, 'a', newline='') as f:
+            f.write(msg)
+            f.write('\n')
+
+    def readFirstNLines(self,n,delim=','):
+        '''
+        Returns the first n lines of self.data_path
+        '''
+        out_list = []
+        with open(self.data_path, 'r', newline='') as f:
+            for i in range(n):
+                read_str = f.readline()
+                if read_str == "": #if EOF reached
+                    break
+                else:
+                    #format data as 2D list
+                    read_str = read_str.replace('\n','') #remove trailing newline
+                    out_list.append(read_str.split(delim))
+
+        self.sig_return.emit(out_list) #return 
+
+    def readLastNLines(self,n,delim=','):
+        with open(self.data_path) as f:
+            lastn = deque(f,n)
+        
+        #formatting data as 2D List
+        out_list = map(lambda x: x.replace('\n',''), lastn)
+        out_list = map(lambda x: x.split(delim),out_list)
+
+        self.sig_return.emit(out_list) #return
+
+    def findFileNum(self,path):
+        '''
+        if path exists append (#) just before file extension
+        '''
+        m = re.match(".*(\.).*$",path)
+        ind_period = m.span(1)[0] #index of period before file extension
+        
+        path0 = path
+        num = 1
+        while(1): 
+            if os.path.exists(path0): 
+                path0 = path[:ind_period] + "({})".format(num) + path[ind_period:]
+                num += 1
+            else:
+                break
+        
+        return path0
+
+    # Accessors for modularity ###############
     def getReturnSig(self) -> qtc.pyqtSignal:
         return self.sig_return
 
-
-
+    def getLogPath(self):
+        return self.data_path
+    ##########################################
 
 class TimestampAxisItem(pg.AxisItem):
     def __init__(self,*args, **kwargs):
