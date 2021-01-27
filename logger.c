bd8cbe8c2fde609dcc9b47f8524d8e0171889622
SyrusVir <jude.alnas@gmail.com>
2021-01-27 00:30:03 -0600

    Logger Main started; buffer operations done; high-level functions still needed

--- logger.c
+++ logger.c
@@ -1,50 +1,68 @@
 #include <logger.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
 
 //logging logic functions
 
 //logger buffer logic functions
-logger_buff_t loggerBufferInit(uint16_t max_size) {
-    logger_buff_t buffer;
-    pthread_mutex_init(&buffer.lock, NULL);
-    pthread_cond_init(&buffer.cond_nonempty, NULL);
-    pthread_cond_init(&buffer.cond_nonfull, NULL);
-    buffer.max_buff_size = max_size;
-    buffer.head = NULL;
-    buffer.tail = NULL;
+logger_buff_t* loggerBufferInit(uint16_t max_size) {
+    logger_buff_t* buffer = (logger_buff_t*) malloc(sizeof(logger_buff_t));
+    pthread_mutex_init(&buffer->lock, NULL);
+    pthread_cond_init(&buffer->cond_nonempty, NULL);
+    pthread_cond_init(&buffer->cond_nonfull, NULL);
+    buffer->max_buff_size = max_size;
+    buffer->head = NULL;
+    buffer->tail = NULL;
 
     return buffer;
 }
 
-void push(logger_buff_t *buffer, logger_buff_node_t *node, bool hi_priority, bool blocking) {
-    
+int push(logger_buff_t *buffer, logger_buff_node_t *new_node, bool hi_priority, bool blocking) {
+    /** Add a node to the linked list buffer. If hi_priority is true, new_node is added at the tail of buffer. 
+     *  Otherwise, new_node is appended at the head. If blocking is true, this function will block if the buffer
+     * is full and will wait until space is available. Otherwise it immediately returns -1.**/
     pthread_mutex_lock(&buffer->lock);
-    if (blocking) {
-        if (buffer->occupancy >= buffer->max_buff_size) {
+    if (buffer->occupancy >= buffer->max_buff_size) {
+        if (blocking) {
             pthread_cond_wait(&buffer->lock, &buffer->cond_nonfull);
+        }
+        else {
+            return -1;
         } 
     } //if desired, block until space available in buffer
     else {
-        if (hi_priority) {
-            node->next = buffer->head;
-            node->prev = NULL;
-            buffer->head = node;
+        if (buffer->head == NULL) {
+            //if the buffer is empty, add appropriately
+            new_node->prev = NULL;
+            new_node->next = NULL;
+            buffer->head = new_node;
+            buffer->tail = new_node;
+        }
+        else if (hi_priority) {
+            // add node at tail
+            new_node->next = NULL;
+            new_node->prev = buffer->tail;
+            buffer->tail->next = new_node;
+            buffer->tail = new_node;
         }
         else {
-            node->next = NULL;
-            node->prev = buffer->tail;
-            buffer->tail->next = node;
-            buffer->tail = node;
+            //add node to head
+            new_node->next = buffer->head; //current head is now second
+            new_node->prev = NULL; //new node has nothing before
+            buffer->head->prev = new_node; //old node points to new node
+            buffer->head = new_node; //head points to new node
         }
     }
     
     buffer->occupancy++;
+    pthread_mutex_unlock(&buffer->lock); //IMPORTANT: Unlock mutex before signaling so waiting threads can acquire lock
     pthread_cond_signal(&buffer->cond_nonempty);
     
-    return;
+    return 0;
 }
-
 
 logger_buff_node_t* pull(logger_buff_t* buffer) {
 /** Returns pointer to last element in list. **/
@@ -58,35 +76,83 @@ logger_buff_node_t* pull(logger_buff_t* buffer) {
     out = buffer->tail; //save output address
     if (buffer->tail->prev == NULL) {
         buffer->tail == NULL;
+        buffer->head == NULL;
     } //if prev == NULL, then current node is only node in list.
     else {
         buffer->tail->prev->next = NULL; //previous node now links to NULL as end of buffer
         buffer->tail = buffer->tail->prev; //buffer tail now points to this node
     } //else there are more nodes in list. Update next-to-last node's pointers accordingly
+    
     buffer->occupancy--;
 
-    pthread_mutex_unlock(&buffer->lock);
+    pthread_mutex_unlock(&buffer->lock); //IMPORTANT: Unlock mutex before signaling so waiting threads can acquire lock
+    pthread_cond_signal(&buffer->cond_nonfull);
 
     return out;
 }
 
-//Mid level buffer interface
-void loggerTryPush(logger_t* logger, logger_buff_node_t* node) {
-    pthread_mutex_lock(&logger->buffer.lock);
-    if (logger->buffer.occupancy == logger->buffer.max_buff_size) {
-        return -1;
-    }
-    else {
+int loggerMain(logger_t* logger) {
+    int status;
+    while (1) {
+        logger_buff_node_t *rec_node = pull(&logger->buffer);
+        logger_msg_t rec_msg = rec_node->msg;
 
+        switch (rec_msg.cmd)
+        {
+        case LOG:
+            FILE *f = fopen(rec_msg.abs_path, 'a');
+            if (f == NULL) {
+                //log error 
+                
+            }
+            break;
+        case CLOSE:
+            /** FREE MEMORY HERE **/
+            return;
+            break;
+        default:
+            break;
+        }
     }
 }
 
 //High level interface functions
 void loggerInit(logger_t* logger, uint16_t buffer_size) {
+    /**Returns a configured logger struct to the buffer pointed to by logger
+     * Calls buffer initialization and create a log file for status reports **/
+    
+    //stat log initialization////////////////////////////
+    char *stat_log_stem = "/logger_logs.txt"; //WARNING: ASSUME UNIX PATH SEPARATOER
+    char *stat_log_root = "./logs";
+    mkdir(stat_log_root, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
+    logger->stat_log_path = (char*) malloc(strlen(stat_log_root)+strlen(stat_log_stem)+); //MEMCHECK 4
+    if (logger->stat_log_path == NULL) {
+        printf("ERROR ALLOCATING MEMORY STAT LOG PATH IN LOGGER INITIALIZATION");
+    }
+    strcpy(logger->stat_log_path,stat_log_root);
+    strcat(logger->stat_log_path,stat_log_stem);
+    logger->stat_log_file = fopen(logger->stat_log_path, 'a');
+
+    //write current time and separator to file
+    char sep[] = '-----------------------------------\n';
+    char timestring[32];
+    struct tm now;
+    time_t rawtime = time(NULL);
+    gmtime_r(&rawtime,&now);
+    strftime(timestring, 30,"%F %T\n",&now);
+    fprintf(logger->stat_log_file,"%s", timestring);
+    fprintf(logger->stat_log_file,"%s", sep);
+    /////////////////////////////////////////////////////////////////
+
+    //Buffer initialization
     logger->buffer = loggerBufferInit(buffer_size);
 }
-void logClose(logger_t* logger) {}
-void logPriorityClose() {}
+
+int logStatus(logger_t logger, char* msg, uint16_t msg_size) {
+
+}
+void loggerClose(logger_t* logger) {}
+void loggerPriorityClose() {}
 int logMsg(logger_t* logger, char* msg, uint16_t msg_size, char* abs_path) {
     //Build logger message
     logger_msg_t logger_msg;
