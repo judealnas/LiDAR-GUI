151a2ecdfd2f3ca63cddeb5e93cbd6216ce6eda6
SyrusVir <jude.alnas@gmail.com>
2021-01-28 02:01:01 -0600

    Added function declarations

--- logger.h
+++ logger.h
@@ -1,6 +1,12 @@
 #include <pthread.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
 
 typedef enum LoggerCommand {
     LOG,
@@ -36,3 +42,18 @@ typedef struct Logger {
     char *stat_log_path;
     FILE *stat_log_file;  
 } logger_t; 
+
+logger_buff_node_t* loggerMsgNodeCreate(logger_cmd_t cmd, char* path, char* data, uint16_t data_size);
+void loggerMsgNodeDestroy(logger_buff_node_t* node);
+logger_buff_t* loggerBufferInit(uint16_t max_size);
+int loggerBufferDestroy(logger_buff_t* buffer);
+int push(logger_buff_t *buffer, logger_buff_node_t *new_node, bool hi_priority, bool blocking);
+logger_buff_node_t* pull(logger_buff_t* buffer);
+void flush(logger_buff_t* buffer);
+int loggerDestroy(logger_t* logger);
+int logStatus(logger_t* logger, char* msg);
+int loggerMain(logger_t* logger);
+logger_t* loggerCreate(uint16_t buffer_size);
+void loggerClose(logger_t* logger, bool hi_priority, bool blocking);
+int loggerMsg(logger_t* logger, char* msg, uint16_t msg_size, char* path, uint16_t path_size);
+int loggerPriorityMsg(logger_t* logger, char* msg, uint16_t msg_size, char* path, uint16_t path_size);
