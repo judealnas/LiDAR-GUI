5bd90dd5aa6d7c4edc73c60fb1b1685891a03b2b
SyrusVir <jude.alnas@gmail.com>
2021-01-28 02:01:35 -0600

    Successful single-threaded test. Threaded operations still need testing.

--- logger_test.c
+++ logger_test.c
@@ -1,7 +1,25 @@
-#include "logger.h"
+#include <logger.h>
+#include <stdlib.h>
 
 int main() {
-    print
+    logger_t* logger = loggerCreate(50);
+    for (int i = 0; i < 10; i++) {
+        char data[5];
+        sprintf(data, "%d", i);
+        logger_buff_node_t* msg = loggerMsgNodeCreate(LOG,"./test.txt",data,sizeof(data));
+        push(logger->buffer, msg, 1, 1);
+        logStatus(logger, "PUSHED");
+    }
+    
 
+    for (int i = 0; i < 6; i++) {
+        logger_buff_node_t* rec = pull(logger->buffer);
+        printf("%s to %s\n",rec->msg.abs_path, rec->msg.data);
+        logStatus(logger,"PULL");
+        loggerMsgNodeDestroy(rec);
+    }
+
+    printf("Buffer Occupancy: %d\n", logger->buffer->occupancy);
+    loggerDestroy(logger);
     return 0;
 }
