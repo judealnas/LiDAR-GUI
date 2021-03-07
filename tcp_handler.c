361237eeae6dd5afaa2076e2547716a954eeb2af
SyrusVir <jude.alnas@gmail.com>
2021-03-06 23:01:19 -0600

    Created tcp_handler.c

---
+++ tcp_handler.c
@@ -0,0 +1,14 @@
+#include "fifo.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+typedef struct TcpHandler {
+    struct socket_addr_in socket_address;
+    pthread_mutex_t
+} tcp_handler_t;
+int tcpHandler()
+{
+
+}
