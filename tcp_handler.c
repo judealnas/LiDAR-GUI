8cf917dc37451c32249de04626d9f7aeb2247203
SyrusVir <jude.alnas@gmail.com>
2021-03-09 01:39:11 -0600

    tcp handler implementation nearly done; fix return values; test state transitions

--- tcp_handler.c
+++ tcp_handler.c
@@ -1,4 +1,5 @@
 #include "fifo.h"
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h>
@@ -6,112 +7,171 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 
+
+typedef enum TcpHandlerCMD {
+    WRITE,
+    DISCONNECT,
+    CLOSE
+} tcp_cmd_t;
+
+typedef enum TcpHandlerState {
+    UNCONNECTED,
+    CONNECTED,
+    ERROR
+} tcp_state_t;
+
+typedef struct TcpHandlerMsg {
+    tcp_cmd_t CMD;
+    char* data;
+    size_t data_len;
+} tcp_msg_t;
+
 typedef struct TcpHandler {
     struct sockaddr_in server_address;  //address to place socket that listens for client connections
-    pthread_mutex_t write_lock;        //mutex to signaling socket is ready for writing
-    fifo_buffer_t* write_buffer;         //fifo buffer holding data to write
+    tcp_state_t tcp_state;              //current state of the tcpHandler; check from producer to see if ready for writes
+    fifo_buffer_t* write_buffer;        //fifo buffer holding data to write
 } tcp_handler_t;
 
+tcp_msg_t* tcpMsgCreate(tcp_cmd_t CMD, char* data, size_t data_len)
+{
+    tcp_msg_t* tcp_msg = (tcp_msg_t*) malloc(sizeof(tcp_msg_t));
+    tcp_msg->data_len = data_len;
+    tcp_msg->data = strndup(data, data_len);
+} //end tcpMsgCreate()
+
+void tcpMsgDestroy(tcp_msg_t* tcp_msg)
+{
+    free(tcp_msg->data);
+    free(tcp_msg);
+} //end tcpMsgDestroy()
+
+int tcpConfigKeepalive(int socket, int idle_time_sec, int num_probes, int probe_intvl_sec)
+{
+    uint8_t val = 1;
+    setsockopt(socket, SOL_SOCKET, SO_KEEPALIVE,&val, sizeof(val));
+    
+    //set idle time in seconds before sending keepalive packets
+    setsockopt(socket, SOL_TCP, TCP_KEEPIDLE, &idle_time_sec, sizeof(idle_time_sec));
+    
+    //Set # of keepalive packets to send before declaring connection dead
+    setsockopt(socket, SOL_TCP, TCP_KEEPCNT, &num_probes, sizeof(num_probes));
+
+    //Set time interval in seconds between keepalive packets
+    setsockopt(socket, SOL_TCP, TCP_KEEPCNT, &probe_intvl_sec, sizeof(probe_intvl_sec));
+}
 
 tcp_handler_t* tcpHandlerInit(struct sockaddr_in server_address)
 {   
     /*  Assumes [server_address] is already configured with the desired port, settings, etc.
         Instantiates the tcpHandler structure for access from main thread and handling thread */
 
-    tcp_handler_t* tcpHandler = (tcp_handler_t*) malloc(sizeof(tcp_handler_t));
+    tcp_handler_t* tcpHandler = (tcp_handler_t*) malloc(sizeof(struct sockaddr_in) + sizeof(tcp_state_t) + sizeof(fifo_buffer_t*));
     tcpHandler->server_address = server_address;
-    pthread_mutex_init(&tcpHandler->write_lock,NULL);
-    pthread_mutex_lock(&tcpHandler->write_lock);   //immediately lock mutex; socket not read for write
+    tcpHandler->tcp_state = UNCONNECTED;
+    tcpHandler->write_buffer = fifoBufferInit(150);
     
     return tcpHandler;
-}
+} //end tcpHandlerInit()
 
-int tcpHandlerMain(tcp_handler_t* tcpHandler)
- {
+tcp_msg_t** tcpHandlerDestroy(tcp_handler_t* tcp_handler) 
+{   
+    tcp_msg_t** leftover = (tcp_msg_t**)fifoBufferClose(tcp_handler->write_buffer);
+    free(tcp_handler);
+    return leftover; 
+} //end tcpHandlerDestroy()
+
+void* tcpHandlerMain(void* tcpHandler_void)
+{
+    //cast void argument to appropriate pointer type
+    tcp_handler_t* tcpHandler = (tcp_handler_t*) tcpHandler_void;
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket  < 0) 
 	{
-		//If any error occurs with socket, stop tcpHandler.
-        //TODO: stopping tcpHandler
-            //1) permanent blocking
-            //2) exit thread (must let parent know somehow)
+		perror("socket() call failed!");
+        return -1;
 	}
 
     // bind server socket to the address
 	if (bind(server_socket, (struct sockaddr*) &tcpHandler->server_address, sizeof(tcpHandler->server_address)) < 0) 
 	{
 		perror("bind() call failed!");
-		exit(1);
+		return -1;
 	}
-
 
     //listen for at most 1 connection
     if (listen(server_socket, 1) < 0) 
 	{
 		perror("listen() failed!");
-		exit(1);
+		return -1;
 	}
     
+    tcp_msg_t* recv_msg;
     while (1) 
     {
-        //wait for an incoming connection; create new client socket upon connection
-        int client_socket;
-        client_socket = accept(server_socket, NULL, NULL);
-        if (client_socket < 0) 
+        switch (tcpHandler->tcp_state)
         {
-            perror("accept() failed");
-            exit(1);
-        }
+            case UNCONNECTED:;
+                //wait for an incoming connection; create new client socket upon connection
+                int client_socket;
+                client_socket = accept(server_socket, NULL, NULL);
+                if (client_socket < 0) 
+                {
+                    perror("accept() failed");
+                    return -1;
+                }
 
-        //TCP Keepalive configuration//////////////////////
-        //enable TCP Keepalive
-        uint8_t val = 1;
-        setsockopt(client_socket, SOL_SOCKET, SO_KEEPALIVE,&val, sizeof(val));
-        
-        //set idle time in seconds before sending keepalive packets
-        val = 30;
-        setsockopt(client_socket, SOL_TCP, TCP_KEEPIDLE, &val, sizeof(val));
-        
-        //Set # of keepalive packets to send before declaring connection dead
-        val = 5;
-        setsockopt(client_socket, SOL_TCP, TCP_KEEPCNT, &val, sizeof(val));
+                //config TCP Keepalive
+                tcpConfigKeepalive(client_socket, 15, 5, 3);
 
-        //Set time interval in seconds between keepalive packets
-        val = 6;
-        setsockopt(client_socket, SOL_TCP, TCP_KEEPCNT, &val, sizeof(val));
-        /////////////////////////////////////////////////////
-        
-        while (1)
-        {
-            char* data = (char*)fifoPull(tcpHandler->write_buffer,1);
+                //state transition
+                tcpHandler->tcp_state = CONNECTED;
+                break;
 
-            int data_len = strlen(data);
+            case CONNECTED:
+                while (tcpHandler->tcp_state == CONNECTED)
+                {
+                    recv_msg = (tcp_msg_t*)fifoPull(tcpHandler->write_buffer,1);
 
-            ssize_t send_status = send(client_socket, data, data_len, MSG_NOSIGNAL);
-			if (send_status >= 0) 
-			{
-				if(send_status < data_len) 
-				{
-					printf("Not all data sent\n");
-				}
-				else 
-				{
-					//printf("Message sent: %s\n",msg);
-				}
-			}
-			else if (errno == EPIPE) 
-			{
-				printf("sending on dead socket. Breaking from loop.\n");
-				break;
-			}
-			else 
-			{
-				perror("Error in send(): ");
-				break;
-			}
+                    switch(recv_msg->CMD)
+                    {
+                        case WRITE:;
+                            ssize_t send_status = send(client_socket,recv_msg->data, recv_msg->data_len, MSG_NOSIGNAL);
+                            if (send_status >= 0) 
+                            {
+                                if(send_status < recv_msg->data_len) 
+                                {
+                                    printf("Not all data sent\n");
+                                }
+                            }
+                            else if (errno == EPIPE) 
+                            {
+                                printf("sending on dead socket. Breaking from loop.\n");
+                                tcpHandler->tcp_state = UNCONNECTED;
+                                break;
+                            }
+                            else 
+                            {
+                                perror("Error in send(): ");
+                                tcpHandler->tcp_state = UNCONNECTED;
+                                break;
+                            }
+                            break;
+                        case DISCONNECT:
+                            close(client_socket);
+                            tcpHandler->tcp_state = UNCONNECTED;
+                            break;
+                        case CLOSE:
+                            
+                            return tcpHandlerDestroy(tcpHandler);
+                            break;
+                        default:
+                            break;
+                    } //end switch(recv_msg->CMD) in CONNECTED case
+                    
+                    tcpMsgDestroy(recv_msg);
 
-        }
-    }
-    
- 
- }
+                } //end while (tcpHandler->tcp_state == CONNECTED)
+                break;
+        } //end switch(tcp_state)
+    } //end while (1) [main loop]
+} //end tcpHandlerMain()
